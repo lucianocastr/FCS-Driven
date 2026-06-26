3af215e235c981ebc66785a0459b12f0c5177c54
lucianocastr <luchianocastro@gmail.com>
2026-06-25 22:58:20 -0300

    release(3.9.0): version bump on integration base (4 surfaces; Assembly/FileVersion frozen 3.0.2.0)

--- src/Fiplex.Control.Software.WinForms/Core/Configuration/VersionCheckService.cs
+++ src/Fiplex.Control.Software.WinForms/Core/Configuration/VersionCheckService.cs
@@ -83,7 +83,7 @@ public class VersionCheckService : IVersionCheckService
         // Split('+')[0] removes the Git hash that .NET adds automatically
         CurrentVersion = (Assembly.GetExecutingAssembly()
             .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
-            ?.InformationalVersion ?? "3.8.1")
+            ?.InformationalVersion ?? "3.9.0")
             .Split('+')[0];
 
         _logger.LogDebug("VersionCheckService initialized. Current version: {Version}", CurrentVersion);
--- src/Fiplex.Control.Software.WinForms/Forms/frmMain.cs
+++ src/Fiplex.Control.Software.WinForms/Forms/frmMain.cs
@@ -116,7 +116,7 @@ public partial class frmMain : Form
     private static readonly string SoftwareVersion =
         (Assembly.GetExecutingAssembly()
             .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
-            ?.InformationalVersion ?? "3.8.1")
+            ?.InformationalVersion ?? "3.9.0")
         .Split('+')[0];
 
     public frmMain(
